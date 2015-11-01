25e27b7cfff5ebcc8a8e00485d3753f13798ded4
Daniella1989 <zdingyue@yahoo.com>
2015-11-01 19:19:23 +0800

    Initial Commit

---
+++ P_CollectionView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  P_CollectionView
+//
+//  Created by dingyuezhang on 11/1/15.
+//  Copyright © 2015 dingyuezhang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
